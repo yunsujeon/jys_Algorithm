f75563b19eb2a12b6a15439d576d5e269e050c66
yunsujeon <dbstndi6316@naver.com>
2020-08-25 21:23:29 +0900

    Update strongly_connected_component_q.cpp

--- strongly_connected_component_q.cpp
+++ strongly_connected_component_q.cpp
@@ -17,6 +17,7 @@
 특정한 그래프안에 포함되어있는 SCC를 출력하는 문제이다. (DFS이용)
 가장 작은 정점의 번호 순으로 출력을 해야하므로 정렬을 수행해줘야한다.
 */
+/*
 #include <iostream>
 #include <stack> //dfs위해
 #include <vector>
@@ -82,6 +83,112 @@ int main() {
 			printf("%d ", SCC[i][j]);
 		}
 		printf("-1\n");
+	}
+	return 0;
+}
+*/
+
+/*4196번 - 도미노
+
+도미노는 연결돼있을때만 쓰러진다. 간혹가다 끊긴 부분이있으면 쓰러지지 않는다.
+모두 쓰러뜨리기 위해 몇 번 건드려 줘야할까?
+
+강한결합요소와 강한결합요소 사이의 관계까지 구해야하는문제
+강한결합요소를 정점으로 보고 위상정렬을 했을 때 진입차수가 0인 정점의 갯수를 세면됨
+모든 SCC에 대해 고유한 번호 값을 매겨 그룹을 만든 뒤 각각의 그룹을
+하나의 정점으로 보고 단순하게 진입차수의 갯수만 세주면 된다.
+이후에 각 그룹에 대해 진입차수의 갯수가 0인 경우에만 카운트를 해주어 출력
+0을 치면 나머지가 넘어가는거기때문
+입력)
+1		테스트케이스의 갯수
+3 2		도미노의 갯수는 3개 관계의 갯수는 2개
+1 2		관계 1 -> 1번 블록이 넘어지면 2번 블록도 넘어진다.
+2 3		관계 2 -> 2번 블록이 넘어지면 3번 블록도 넘어진다.
+출력) 1*/
+
+#include <iostream>
+#include <stack>
+#include <vector>
+
+#define MAX 100001
+
+using namespace std;
+
+int n, m;
+int id, d[MAX];
+bool finished[MAX];
+vector<int> a[MAX];
+vector<vector<int> > SCC;
+stack<int> s;
+int group[MAX];
+bool inDegree[MAX]; //각각의 정점의 진입차수가 몇인지 확인
+
+int dfs(int x) {
+	d[x] = ++id;
+	s.push(x);
+
+	int parent = d[x];
+	for (int i = 0; i < a[x].size(); i++) {
+		int y = a[x][i];
+		if (d[y] == 0)
+			parent = __min(parent, dfs(y)); //또는 #define NOMINMAX 추가해주면됨
+		else if (!finished[y])
+			parent = __min(parent, d[y]);
+	}
+	if (parent == d[x]) {
+		vector<int> scc;
+		while (1) {
+			int t = s.top();
+			s.pop();
+			scc.push_back(t);
+			group[t] = SCC.size() + 1; //특정한 SCC에 속해있는 모든 
+			//정점들이 SCC크기에 1을 더해준값을 넣어주는것
+			//첫번째 SCC에 들어있는 애들은 1일것이고
+			//두번째 SCC에 들어있는 애들은 2를 가질것이다.
+			//그룹값을 기준으로 어떤 SCC에 포함되어있는지 구별할수있다.
+			finished[t] = true;
+			if (t == x)
+				break;
+		}
+		SCC.push_back(scc);
+	}
+	return parent;
+}
+
+int main() {
+	int t;
+	scanf_s("%d", &t);
+	while (t--) {
+		SCC.clear();
+		fill(d, d + MAX, 0); //d값을 초기화한다.
+		fill(finished, finished + MAX, 0); //끝났는지 판별 다 초기화 0
+		fill(inDegree, inDegree + MAX, 0); //진입차수 다 초기화 0
+		scanf_s("%d %d", &n, &m); //정점과 간선정보
+		for (int i = 1; i <= n; i++) { 
+			a[i].clear();
+		}
+		for (int i = 0; i < m; i++) {
+			int x, y;
+			scanf_s("%d %d", &x, &y);
+			a[x].push_back(y);
+		}
+		for (int i = 1; i <= n; i++) {//모든 노드에 대해서 dfs
+			if (d[i] == 0)
+				dfs(i);
+		}
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j < a[i].size(); j++) {
+				int y = a[i][j]; //연결되어진 정점을 y로 함
+				if (group[i] != group[y]) //y와 i가 속한 그룹이 다르다면
+					inDegree[group[y]] = true; //y가 속한 SCC의 그룹 값을 true로 즉 진입차수가 존재한다.
+			}
+		}
+		int result = 0;
+		for (int i = 1; i <= SCC.size(); i++) { //SCC들을 확인하면서
+			if (!inDegree[i]) //진입차수가 0인경우는
+				result++; //갯수 증가시킨다.
+		}
+		printf("%d\n", result);
 	}
 	return 0;
 }
