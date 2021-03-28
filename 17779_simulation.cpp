e83e97aaf02bc8a1d8f1d41787cc894968116c93
yunsujeon <dbstndi6316@naver.com>
2021-03-28 23:36:37 +0900

    Update 17779_simulation.cpp

--- 17779_simulation.cpp
+++ 17779_simulation.cpp
@@ -1,2 +1,18 @@
-//개리맨더링 2
-//
+//게리맨더링
+//BF로 풀수있을듯?
+//아니면 dfs와 백트래킹
+#include <iostream>
+
+int part() { //각 파트별 넓이가 얼마인지 구하고 최소를 리턴
+
+}
+
+
+int solution() { //BF
+
+}
+int main() {
+
+	solution();
+	return 0;
+}
